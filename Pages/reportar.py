56425eb75bac18cfd82d32bbcf3d4ab6fc8381f5
laupontiroli <laurapm@al.insper.edu.br>
2023-10-31 09:27:44 -0300

    reportar em andamento

--- Pages/reportar.py
+++ Pages/reportar.py
@@ -1,4 +1,11 @@
 import streamlit as st 
+import os 
+import api_utils as uts
+
+def save_uploaded_file(uploadedfile):
+  with open(os.path.join("./photos",uploadedfile.name),"wb") as f:
+     f.write(uploadedfile.getbuffer())
+  return st.success("Saved file :{} in photos".format(uploadedfile.name))
 
 st.title('Informações sobre o Problema')
 
@@ -10,6 +17,7 @@ with col1:
     bairro = st.text_input("Digite o Nome do Bairro",placeholder="Nome do Bairro")
     rua = st.text_input("Digite o Nome da Rua",placeholder="Nome da rua")
     foto = st.file_uploader("Foto do problema")
+    
 
 with col2: 
     problema_tipo = st.selectbox("Tipo de problema",("Buraco na via","buraco na calçada","luz queimada",'farol queimado','outro'))
@@ -20,7 +28,12 @@ with col2:
     data_inicio = st.date_input("Desde quando este problema existe ou desde quando você o percebeu?",format='DD/MM/YYYY')
 
 
-st.button("Enviar Formulario")
-
+if st.button("Enviar Formulario"):
+    if foto:
+            save_uploaded_file(foto)
+    data = {'bairro': bairro,'rua': rua, 'problema_tipo':problema_tipo, 'urgencia': urgencia, 'problema_descricao': problema_descricao,'data_inicio':data_inicio}
+    file= {'foto':open(f'./photos/{foto.name}','rb')}
+    uts.registra_problema(data,file)
+    
 
 
