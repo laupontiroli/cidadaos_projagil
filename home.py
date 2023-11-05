2f9f158f97b125cd3f53cdb06200af46e9fa86ef
Rafaela Afférri de Oliveira <rafaelaao@al.insper.edu.br>
2023-11-05 18:08:04 -0300

    home terminada

--- home.py
+++ home.py
@@ -3,6 +3,9 @@ from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 
+
+st.set_page_config(page_title='Reporta Cidade', page_icon=':cityscape:', layout='wide', initial_sidebar_state='auto')
+
 loc_button = Button(label="Get Location")
 loc_button.js_on_event("button_click", CustomJS(code="""
     navigator.geolocation.getCurrentPosition(
@@ -22,3 +25,23 @@ result = streamlit_bokeh_events(
 if result:
     if "GET_LOCATION" in result:
         st.write(result.get("GET_LOCATION"))
+
+import streamlit as st
+from utils import *
+
+
+st.title('Reporta Cidade')
+
+st.sidebar.title('Menu')
+
+st.write('## Algum problema na cidade para reportar?')
+
+st.write("""Buracos nas ruas e avenidas?""")
+st.write(' Falta de sinalização?')
+st.write(' Postes queimados?')
+st.write('Calçadas obstruídas?')
+st.write('Vazamento de água ou esgoto? ')
+st.write(' Falta de rampas para deficientes?')
+st.write('...')
+
+st.write('### Para reportar clique em "reportar" na aba do menu!')
