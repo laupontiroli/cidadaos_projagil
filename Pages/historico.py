d7d0477e9146ae758b96db0ff225c271f27cfa20
laupontiroli <laurapm@al.insper.edu.br>
2023-11-06 14:24:22 -0300

    busca do problema pelo id funcionando

--- Pages/historico.py
+++ Pages/historico.py
@@ -1,18 +1,18 @@
 import streamlit as st
+from api_utils import get_problema_id
+# def obter_info_reclamacao(codigo):
+#     reclamacoes = {
+#         "12345": {
+#             "resumo": "Infraestrutura danificada",
+#             "descricao": "Buraco com raio de 1m no meio da rua",
+#             "localizacao": "Rua Quata",
+#             "status": "Em andamento",
+#             "timeline": [
+#                 {"data": "01/10/2023", "hora": "09:00:00", "acao": "Reclamação registrada"},
+#                 {"data": "05/10/2023", "hora": "10:30:00", "acao": "Reclamação em análise pelos órgãos públicos"},
+#                 {"data": "10/10/2023", "hora": "11:45:00", "acao": "Trabalhadores foram enviados ao local"} ]}}
 
-def obter_info_reclamacao(codigo):
-    reclamacoes = {
-        "12345": {
-            "resumo": "Infraestrutura danificada",
-            "descricao": "Buraco com raio de 1m no meio da rua",
-            "localizacao": "Rua Quata",
-            "status": "Em andamento",
-            "timeline": [
-                {"data": "01/10/2023", "hora": "09:00:00", "acao": "Reclamação registrada"},
-                {"data": "05/10/2023", "hora": "10:30:00", "acao": "Reclamação em análise pelos órgãos públicos"},
-                {"data": "10/10/2023", "hora": "11:45:00", "acao": "Trabalhadores foram enviados ao local"} ]}}
-
-    return reclamacoes.get(codigo, None)
+#     return reclamacoes.get(codigo, None)
 
 
 st.markdown("<h1 id='inicio' class='titulo'>Acompanhe a resolução de algum problema</h1>", unsafe_allow_html=True)
@@ -34,19 +34,20 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 codigo_reclamacao = st.text_input("Insira o código da reclamação:")
-info_reclamacao = obter_info_reclamacao(codigo_reclamacao)
-
-if info_reclamacao:
+achou,info_reclamacao =get_problema_id(codigo_reclamacao)
+print (f'-----------------------{info_reclamacao}')
+if achou:
     st.markdown("<h2 class='subtitulo'>Detalhes da reclamação</h2>", unsafe_allow_html=True)
-    st.write(f"Resumo do problema: {info_reclamacao['resumo']}")
-    st.write(f"Descrição: {info_reclamacao['descricao']}")
-    st.write(f"Localização: {info_reclamacao['localizacao']}")
-    st.write(f"Status: {info_reclamacao['status']}")
+    st.write(f"Descrição do Problema : {info_reclamacao['problemas'][0]['problema_descricao']}")
+    st.write(f"O tipo de problema: {info_reclamacao['problemas'][0]['problema_tipo']}")
+    st.write(f"Localização: {info_reclamacao['problemas'][0]['rua']},{info_reclamacao['problemas'][0]['bairro']}")
+    st.write(f"Status: {info_reclamacao['problemas'][0]['status']}")
     
-    st.markdown("<h2 class='subtitulo'>Timeline da reclamação</h2>", unsafe_allow_html=True)
-    for evento in info_reclamacao['timeline']:
-        st.write(f"Dia: {evento['data']} - Hora: {evento['hora']} - Ação: {evento['acao']}")
+    # st.markdown("<h2 class='subtitulo'>Timeline da reclamação</h2>", unsafe_allow_html=True)
+    # for evento in info_reclamacao['timeline']:
+    #     st.write(f"Dia: {evento['data']} - Hora: {evento['hora']} - Ação: {evento['acao']}")
 else:
-    if codigo_reclamacao:
-        st.error("Reclamação não encontrada. Verifique o código inserido.")
+    # if codigo_reclamacao:
+    #     st.error("Reclamação não encontrada. Verifique o código inserido.")
+    st.write(info_reclamacao)
 
--- api_utils.py
+++ api_utils.py
@@ -9,3 +9,12 @@ def registra_problema(data,file):
         return True, response.json()
     except Exception as e:
         return False, {"message": str(e)}
+    
+def get_problema_id(id):
+    try:
+        json = {"_id":str(id)}
+        response = requests.get(f"{BASE_URL}/problemas/filter",json=json)
+        response.raise_for_status()
+        return True, response.json()
+    except Exception as e:
+        return False, {"message": str(e)}
