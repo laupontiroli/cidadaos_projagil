1f051ef6e15855661ab82e142fdf2c9a0977c3d8
laupontiroli <laurapm@al.insper.edu.br>
2023-11-05 21:23:21 -0300

    ajustes

--- home.py
+++ home.py
@@ -1,37 +1,7 @@
 import streamlit as st
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
-
-
-st.set_page_config(page_title='Reporta Cidade', page_icon=':cityscape:', layout='wide', initial_sidebar_state='auto')
-
-loc_button = Button(label="Get Location")
-loc_button.js_on_event("button_click", CustomJS(code="""
-    navigator.geolocation.getCurrentPosition(
-        (loc) => {
-            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
-        }
-    )
-    """))
-result = streamlit_bokeh_events(
-    loc_button,
-    events="GET_LOCATION",
-    key="get_location",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
-
-if result:
-    if "GET_LOCATION" in result:
-        st.write(result.get("GET_LOCATION"))
-
-import streamlit as st
 from utils import *
 
-
 st.title('Reporta Cidade')
-
 st.sidebar.title('Menu')
 
 st.write('## Algum problema na cidade para reportar?')
